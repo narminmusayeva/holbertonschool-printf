7eb4cc453b3c76ef1103ab1c1f38990ce91fd94e
narminmusayeva <narminmsyva@gmail.com>
2024-11-30 18:15:34 +0400

    completed printf function

--- _printf.c
+++ _printf.c
@@ -1,45 +1,44 @@
-#include "main.h"
 #include <stdarg.h>
+#include "main.h"
 
 /**
  * _printf - A simplified version of the printf function
- * @format: format string that can contain specifiers like %c, %s, %%
+ * @format: format string with specifiers like %c, %s, and %%.
  *
- * Return: the number of characters printed (excluding the null byte)
+ * Description: Prints formatted output to stdout. Supports %c, %s, and %%.
+ *
+ * Return: Number of characters printed, or -1 on error.
  */
-
 int _printf(const char *format, ...)
 {
-
 unsigned int len = 0;
 int i = 0;
 va_list args;
-va_start (args, format);
+va_start(args, format);
 while (format[i])
-
 {
-
 if (format[i] == '%' && format[i + 1] == 'c')
-len += _putchar(va_arg(args, int)), i += 2;
-
-else if (format[i] == '%' && format[i+1] == 's')
-
-len += print_string(va_arg(args, char *)), i += 2;
-
+{
+len += _putchar(va_arg(args, int));
+i += 2;
+}
+else if (format[i] == '%' && format[i + 1] == 's')
+{
+len += print_string(va_arg(args, char *));
+i += 2;
+}
 else if (format[i] == '%' && format[i + 1] == '%')
-len += _putchar('%'), i +=2;
-
+{
+len += _putchar('%');
+i += 2;
+}
 else
 {
-
 len += _putchar(format[i]);
 i++;
-
 }
-
 }
-
 va_end(args);
-return len;
-
+return (len);
 }
+
